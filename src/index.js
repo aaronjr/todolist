16e6ea82c419cc5100e11694165c81cbe3e39f11
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-11-20 16:31:37 +0000

    minor changes to CSS

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import { Manager } from "./projectManager";
 import { layout } from "./layout"
 import './styles.css';
 
-let proj = new Project("shop", "carry out early morning", "tomorrow");
-let task = new Task("milk", "green", "ASAP");
-let task1 = new Task("cheese", "blue", "ASAP");
+let proj = new Project("Shop", "Shop in ASDA", "This week");
+let task = new Task("Milk", "Green", "");
+let task1 = new Task("Cheese", "Blue", "");
 
 // hide one task
 task.outstanding = false
 
-let proj2 = new Project("garden", "carry out tonight", "tonight")
-let task2 = new Task("mow", "grass", "ASAP")
-let task12 = new Task("cut", "tree", "soon")
+let proj2 = new Project("Garden", "Carry out tonight", "Tonight")
+let task2 = new Task("Mow", "Grass out of the back", "ASAP")
+let task12 = new Task("Cut", "Tree", "soon")
 
 // create holder of projects
 const manager = new Manager
